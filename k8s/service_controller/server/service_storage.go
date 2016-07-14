1ef7978d2f481f83c24db8f99fe2977fe5b9421e
Morgan Bauer <mbauer@us.ibm.com>
2016-07-14 12:27:07 -0700

    split up the struct by domain
     - also ensure we satisfy the interface
    
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>

--- k8s/service_controller/server/service_storage.go
+++ k8s/service_controller/server/service_storage.go
@@ -12,21 +12,38 @@ type ServiceBroker struct {
 	Password string `json:password""`
 }
 
-type ServiceStorage interface {
+// The Broker interface provides functions to deal with brokers.
+type Broker interface {
 	ListBrokers() ([]*ServiceBroker, error)
 	GetBroker(string) (*ServiceBroker, error)
 	GetInventory(string) (*model.Catalog, error)
 	AddBroker(*ServiceBroker, *model.Catalog) error
 	DeleteBroker(string) error
+}
 
+// The Instancer interface provides functions to deal with service instances.
+type Instancer interface {
 	ListServices(string) ([]*model.ServiceInstance, error)
 	GetService(string, string) (*model.ServiceInstance, error)
 	ServiceExists(string, string) bool
 	AddService(string, *model.ServiceInstance) error
 	DeleteService(string, string) error
+}
 
+// The Binder interface provides functions to deal with service
+// bindings.
+type Binder interface {
 	ListServiceBindings(string, string) ([]*model.ServiceBinding, error)
 	GetServiceBinding(string, string, string) (*model.Credential, error)
 	AddServiceBinding(string, *model.ServiceBinding, *model.Credential) error
 	DeleteServiceBinding(string, string, string) error
+}
+
+// The ServiceStorage interface provides a comprehensive combined
+// resource for end to end dealings with service brokers, service instances,
+// and service bindings.
+type ServiceStorage interface {
+	Broker
+	Instancer
+	Binder
 }
